4439d915d5b4000f23de5d379dbdef54724482f7
kregter <pihteevn@ru-drive.com>
2026-03-24 20:29:54 +0300

    add homework6.1.1, 6.1.2

---
+++ homework6.1/homework6.1.1/homework6.1.1.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <thread>
+#include <functional>
+
+void incr_queue(int& queue_count, int max_queue) {
+	while (queue_count < max_queue) {
+		std::this_thread::sleep_for(std::chrono::seconds(1));
+		(queue_count)++;
+		std::cout << "Текущая очередь ++ = " << queue_count << std::endl;
+	}
+}
+
+void decr_queue(int& queue_count) {
+	while (queue_count > 0) {
+		std::this_thread::sleep_for(std::chrono::seconds(2));
+		(queue_count)--;
+		std::cout << "Текущая очередь -- = " << queue_count << std::endl;
+	}
+}
+
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	
+	int max_queue = 6;
+	int queue_count = 1;
+	
+	std::thread t1(incr_queue, std::ref(queue_count), max_queue);
+	std::thread t2(decr_queue, std::ref(queue_count));
+	t1.join();
+	t2.join();
+	
+	return 0;
+}
---
+++ homework6.1/homework6.1.2/homework6.1.2.cpp
@@ -0,0 +1,57 @@
+﻿#include <iostream>
+#include <thread>
+#include <vector>
+#include <chrono>
+
+void sum(std::vector<int>& vec1, std::vector<int>& vec2, std::vector<int>& result, int from, int to)
+{
+    for (int i = from; i < to; i++)
+    {
+        result[i] = vec1[i] + vec2[i];
+    }
+}
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    std::cout << "Количество аппаратных ядер - " << std::thread::hardware_concurrency() << std::endl;
+
+    std::cout << "\t\t1000" << " \t\t" << "10000" << " \t\t" << "100000" << " \t\t" << "1000000" << std::endl;
+
+    int vector_length = 1000;
+    int threads_count = 1;
+
+    for (threads_count = 1; threads_count <= 16; threads_count *= 2)
+    {
+        std::cout << threads_count << " Поток  \t";
+        for (vector_length = 1000; vector_length <= 1000000; vector_length *= 10)
+        {
+            std::vector<int> a(vector_length);
+            std::vector<int> b(vector_length);
+            std::vector<int> result(vector_length);
+
+            std::vector<std::thread> threads;
+
+            auto start = std::chrono::steady_clock::now();
+
+            int delta = vector_length / threads_count;
+            for (int i = 0; i < threads_count; ++i) {
+                int start = i * delta;
+                int end = (i + 1 == threads_count) ? vector_length : start + delta;
+                threads.emplace_back(sum, std::ref(a), std::ref(b), std::ref(result), start, end);
+            }
+
+            for (auto& th : threads)
+            {
+                th.join();
+            }
+
+            auto end = std::chrono::steady_clock::now();
+            std::chrono::duration<double, std::milli> duration = end - start;
+
+            std::cout << duration.count() << " ms\t";
+        }
+        std::cout << std::endl;
+    }
+    return 0;
+}
