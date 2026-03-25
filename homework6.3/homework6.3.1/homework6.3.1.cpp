4a7498513e8abd3c527015bbe08d82707270367a
kregter <pihteevn@ru-drive.com>
2026-03-25 19:17:03 +0300

    add homework6.3.1, 6.3.2

---
+++ homework6.3/homework6.3.1/homework6.3.1.cpp
@@ -0,0 +1,62 @@
+﻿#include <iostream>
+#include <future>
+#include <list>
+#include <thread>
+
+void find_min(std::promise<std::list<int>::iterator>&& prom,
+    std::list<int>::iterator start,
+    std::list<int>::iterator end)
+{
+    auto min_it = start;
+    for (auto it = start; it != end; ++it)
+    {
+        if (*it < *min_it)
+        {
+            min_it = it;
+        }
+    }
+    prom.set_value(min_it);
+}
+
+void my_sort(std::list<int>& lst)
+{
+    for (auto it = lst.begin(); it != lst.end(); ++it)
+    {
+        std::promise<std::list<int>::iterator> prom;
+        std::future<std::list<int>::iterator> fut = prom.get_future();
+
+        std::thread t(find_min,
+            std::move(prom),
+            it,
+            lst.end());
+
+        auto min_it = fut.get();
+        if (min_it != it)
+        {
+            std::swap(*it, *min_it);
+        }
+
+        t.join();
+    }
+}
+
+int main()
+{
+    std::list<int> lst = { 5, 3, 8, 1, 4 };
+
+    std::cout << "Initial list: ";
+    for (const auto& val : lst)
+    {
+        std::cout << val << " ";
+    }
+    std::cout << '\n';
+
+    my_sort(lst);
+
+    std::cout << "Sorted list: ";
+    for (const auto& val : lst)
+    {
+        std::cout << val << " ";
+    }
+    std::cout << '\n';
+}
---
+++ homework6.3/homework6.3.2/homework6.3.2.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+#include <list>
+#include <algorithm>
+#include <numeric>
+#include <future>
+
+template<typename Iterator, typename Function>
+void parallel_for_each(Iterator first, Iterator end, Function func)
+{
+    size_t size = std::distance(first, end);
+    if (size < 2)
+    {
+        std::for_each(first, end, func);
+        return;
+    }
+    else
+    {
+        Iterator mid = first;
+        std::advance(mid, size / 2);
+
+        std::async(parallel_for_each<Iterator, Function>, first, mid, func);
+        parallel_for_each(mid, end, func);
+    }
+}
+
+
+int main()
+{
+    std::list<int> lst = { 5, 3, 8, 1, 4 };
+
+    std::cout << std::accumulate(lst.begin(), lst.end(), 0) << std::endl;
+
+    for (const auto& val : lst)
+    {
+        std::cout << val << " ";
+    }
+    std::cout << '\n';
+
+    parallel_for_each(lst.begin(), lst.end(), [](int& n) { n *= 2; });
+
+    for (const auto& val : lst)
+    {
+        std::cout << val << " ";
+    }
+    std::cout << '\n';
+}
